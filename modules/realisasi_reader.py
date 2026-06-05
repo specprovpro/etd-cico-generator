b8ac966ab7382714276021a1429070716d617f14
specprovpro <specprovpro@gmail.com>
2026-06-05 16:28:59 +0700

    Update realisasi_reader.py

--- modules/realisasi_reader.py
+++ modules/realisasi_reader.py
@@ -1,1 +1,25 @@
+import pandas as pd
 
+
+def read_realisasi(file):
+
+    df = pd.read_excel(
+        file,
+        header=0
+    )
+
+    # Header sebenarnya ada di baris pertama
+    df.columns = [
+        str(x).strip()
+        for x in df.iloc[0]
+    ]
+
+    # Buang baris header duplikat
+    df = df.iloc[1:].reset_index(drop=True)
+
+    # Buang baris kosong
+    df = df.dropna(
+        how="all"
+    )
+
+    return df
